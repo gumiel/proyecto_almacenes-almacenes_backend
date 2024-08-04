8e3db9e300a80fed9095bc78782a00839f458802
henry perez <prez.gumiel@gmail.com>
2024-08-04 19:53:43 -0400

    correcciones

--- src/main/java/com/gestion/almacenes/controllers/PackingController.java
+++ src/main/java/com/gestion/almacenes/controllers/PackingController.java
@@ -4,6 +4,7 @@ import com.gestion.almacenes.commons.util.PagePojo;
 import com.gestion.almacenes.dtos.PackingDto;
 import com.gestion.almacenes.entities.Packing;
 import com.gestion.almacenes.services.PackingService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
@@ -23,23 +24,26 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/packing")
-@Tag(name = "PackingController")
+@Tag(name = "Gestión de empaque (PackingController)")
 public class PackingController {
 
   private final PackingService packingService;
 
+  @Operation(summary = "Obtener todos los registros")
   @GetMapping
   public ResponseEntity<List<Packing>> getAll() {
     List<Packing> packings = packingService.getAll();
     return ResponseEntity.status(HttpStatus.OK).body(packings);
   }
 
+  @Operation(summary = "Creación del registro")
   @PostMapping
   public ResponseEntity<Packing> create(@Valid @RequestBody PackingDto dto) {
     Packing packingSaved = packingService.create(dto);
     return ResponseEntity.status(HttpStatus.CREATED).body(packingSaved);
   }
 
+  @Operation(summary = "Edición del registro")
   @PutMapping("/{id}")
   public ResponseEntity<Packing> update(@PathVariable Integer id,
       @Valid @RequestBody PackingDto dto) {
@@ -47,18 +51,21 @@ public class PackingController {
     return ResponseEntity.status(HttpStatus.CREATED).body(packingUpdated);
   }
 
+  @Operation(summary = "Obtención de los datos del registro por el identificador")
   @GetMapping("/{id}")
   public ResponseEntity<Packing> getById(@PathVariable Integer id) {
     Packing packing = packingService.getById(id);
     return ResponseEntity.status(HttpStatus.OK).body(packing);
   }
 
+  @Operation(summary = "Eliminación del registro por el identificador")
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> delete(@PathVariable Integer id) {
     packingService.delete(id);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 
+  @Operation(summary = "Buscador de registros por atributos")
   @GetMapping("/search")
   public ResponseEntity<List<Packing>> getFiltered(
       @RequestParam(required = false) String code,
@@ -68,6 +75,7 @@ public class PackingController {
     return ResponseEntity.status(HttpStatus.OK).body(packingListFiltered);
   }
 
+  @Operation(summary = "Paginador y buscador de registros por atributos")
   @GetMapping("/pageable")
   public ResponseEntity<PagePojo<Packing>> getAllPagination(
       @RequestParam(defaultValue = "0") int page,
